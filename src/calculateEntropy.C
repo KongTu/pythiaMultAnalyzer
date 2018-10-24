9220565d47c460db352fdf0318f3a1241b8a0d48
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2018-10-24 19:04:30 -0400

    add

--- src/calculateEntropy.C
+++ src/calculateEntropy.C
@@ -20,19 +20,32 @@ void calculateEntropy(){
 
 
 	TFile* file = new TFile("../rootfiles/100k_CT10_pythia_multiplicity.root");
-	TH1D* Ntrk_gen = (TH1D*) file->Get("Nch_gen");
+	TH1D* Nch_gen_target = (TH1D*) file->Get("Nch_gen_target");
+	TH1D* Nch_gen_current = (TH1D*) file->Get("Nch_gen_current");
 	
-	Ntrk_gen->Scale(1.0/Ntrk_gen->Integral());
-	Ntrk_gen->Draw();
+	Nch_gen_target->Scale(1.0/Nch_gen_target->Integral());
+	Nch_gen_current->Scale(1.0/Nch_gen_current->Integral());
+	
+	Nch_gen_current->Draw();
 
-	double entropy = 0.0;
-	for(int i = 0; i < Ntrk_gen->GetNbinsX(); i++){
+	double entropy_target = 0.0;
+	for(int i = 0; i < Nch_gen_target->GetNbinsX(); i++){
 
-		double pn = Ntrk_gen->GetBinContent(i+1);
+		double pn = Nch_gen_target->GetBinContent(i+1);
 		if( pn == 0. ) continue;
-		entropy += (-pn)*TMath::Log(pn);
+		entropy_target += (-pn)*TMath::Log(pn);
 	}
 
-	cout << "entropy is " << entropy << endl;
+	cout << "entropy_target is " << entropy_target << endl;
+
+	double entropy_current = 0.0;
+	for(int i = 0; i < Nch_gen_current->GetNbinsX(); i++){
+
+		double pn = Nch_gen_current->GetBinContent(i+1);
+		if( pn == 0. ) continue;
+		entropy_current += (-pn)*TMath::Log(pn);
+	}
+
+	cout << "entropy_current is " << entropy_current << endl;
 
 }
