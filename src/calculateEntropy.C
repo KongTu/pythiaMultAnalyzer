fa1c0e493c8ee62c9689897d54cc90026c47a67e
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2018-10-24 18:39:28 -0400

    add

---
+++ src/calculateEntropy.C
@@ -0,0 +1,23 @@
+#include <memory>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <map>
+#include <sstream>
+#include <iomanip>
+
+#include "RiceStyle.h"
+
+using namespace std;
+
+
+void calculateEntropy(){
+
+
+	TFile* file = new TFile("../rootfiles/100k_CT10_pythia_multiplicity.root");
+	TH1D* Ntrk_gen = (TH1D*) file->Get("Ntrk_gen");
+	Ntrk_gen->Draw();
+
+}
