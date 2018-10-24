ad016fcb4ccef8be4b2ad0af58f00ab33ce99c82
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2018-10-24 18:49:47 -0400

    add

--- src/calculateEntropy.C
+++ src/calculateEntropy.C
@@ -11,7 +11,7 @@
 #include "TFile.h"
 #include "TH1.h"
 #include "TCanvas.h"
-
+#include "TMath.h"
 
 using namespace std;
 
@@ -21,6 +21,17 @@ void calculateEntropy(){
 
 	TFile* file = new TFile("../rootfiles/100k_CT10_pythia_multiplicity.root");
 	TH1D* Ntrk_gen = (TH1D*) file->Get("Nch_gen");
-	Ntrk_gen->Draw();
+	
+	Ntrk_gen->Scale(1.0/Ntrk_gen->Integral());
+
+	double entropy = 0.0;
+	for(int i = 0; i < Ntrk_gen->GetNbinsX(); i++){
+
+		double pn = Ntrk_gen->GetBinContent(i+1);
+
+		entropy = entropy + -pn*TMath::Log(pn);
+	}
+
+	cout << "entropy is " << entropy << endl;
 
 }
