d8882c37141088e70fa5b776950ec3b88150c96c
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2018-10-24 18:51:06 -0400

    add

--- src/calculateEntropy.C
+++ src/calculateEntropy.C
@@ -23,13 +23,14 @@ void calculateEntropy(){
 	TH1D* Ntrk_gen = (TH1D*) file->Get("Nch_gen");
 	
 	Ntrk_gen->Scale(1.0/Ntrk_gen->Integral());
+	Ntrk_gen->Draw();
 
 	double entropy = 0.0;
 	for(int i = 0; i < Ntrk_gen->GetNbinsX(); i++){
 
 		double pn = Ntrk_gen->GetBinContent(i+1);
 
-		entropy = entropy + -pn*TMath::Log(pn);
+		entropy += (-pn)*TMath::Log(pn);
 	}
 
 	cout << "entropy is " << entropy << endl;
