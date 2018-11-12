fa648a10c9407e1c621b29a5b22ee5f5ab1c8f88
KongTu <kongkong@Kongs-Mac-mini.fios-router.home>
2018-11-12 15:58:32 -0500

    adding new 100 files

--- src/pythiaMultAnalyzer.C
+++ src/pythiaMultAnalyzer.C
@@ -237,7 +237,7 @@ void pythiaMultAnalyzer(int nEvents, TString inputFilename ){
 		int struck_nucleon = event->nucleon;
 		
 		if( event_process != 99 ) continue;
-		if( trueQ2 < 10 || trueQ2 > 11 ) continue;
+		if( trueQ2 < 15 || trueQ2 > 16 ) continue;
 		if( trueX < 0.00001 || trueX > 0.0011 ) continue;
 		
 		int x_index = -1;
