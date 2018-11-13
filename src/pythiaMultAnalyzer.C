0eee682e6501d6430a5dae9958e81a14139db043
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2018-11-13 14:53:37 -0500

    use pT > 0.1

--- src/pythiaMultAnalyzer.C
+++ src/pythiaMultAnalyzer.C
@@ -327,12 +327,12 @@ void pythiaMultAnalyzer(int nEvents, TString inputFilename ){
 			etaStar_gen->Fill( part4vStar.Eta() );
 			eta_gen->Fill( eta );
 
-			if( part4vStar.Pt() < 0.0 ) continue;
+			if( part4vStar.Pt() < 0.1 ) continue;
 			
-			if( part4vStar.Eta() < -2.0 ) {
+			if( part4vStar.Eta() < 0.0 ) {
 				nParticles_process++;
 			}
-			if( part4vStar.Eta() > 2.0){
+			if( part4vStar.Eta() > 0.0){
 				nParticles_process_current++;
 			}
 
