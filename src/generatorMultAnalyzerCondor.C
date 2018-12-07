11226b9d3583b9dffa41f969a155b2478d68e243
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2018-12-07 17:23:35 -0500

    update pythia code

--- src/generatorMultAnalyzerCondor.C
+++ src/generatorMultAnalyzerCondor.C
@@ -180,12 +180,13 @@ void generatorMultAnalyzerCondor(int nEvents, TString inputName, TString outputN
 
 			if( part4vStar.Pt() < 0.0 ) continue;
 			
-			nParticles_all++;
-
-			if( part4vStar.Eta() < 0.0 && part4vStar.Eta() > -1.5 ) {
+			if( part4vStar.Eta() < 4.0 && part4vStar.Eta() > -4.0 ){
+				nParticles_all++;
+			}
+			if( part4vStar.Eta() < 0.0 && part4vStar.Eta() > -4.0 ) {
 				nParticles_process++;
 			}
-			if( part4vStar.Eta() > 0.0 && part4vStar.Eta() < 1.5){
+			if( part4vStar.Eta() > 0.0 && part4vStar.Eta() < 4.0){
 				nParticles_process_current++;
 			}
 
